f6acd538912f80bc306efb3136c75c6846e3f5e8
Andre Lucas <andre.lucas284@gmail.com>
2021-03-12 20:11:39 -0500

    quiz

--- questions.js
+++ questions.js
@@ -1,28 +1,28 @@
 const questions = [
     {
-        title: "Question 1: Commonly used data types DO NOT include:",
+        title: "Question 1: Which method would you use to find an ID element?",
+        choices: ["getElementsById()", "getElementByID()", "getElementbyId()", "getElementsById()"],
+        answer: "getElementbyId()"
+    },
+    {
+        title: "Question 2: To see if two variables are equal in an if / else statement you would use ____.",
+        choices: ["=", "==", "equals", "!="],
+        answer: "=="
+    },
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "quotes"
+    },
+    {
+        title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        answer: "console log"
+    },
+    {
+        title: "Commonly used data types DO NOT include:",
         choices: ["strings", "booleans", "alerts", "numbers"],
         answer: "alerts"
-    },
-    {
-        title: "Question 2: The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
-    },
-    {
-        title: "Question 3: The instructions for a function are enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "curly brackets"
-    },
-    {
-        title: "Question 4: A property of an object that is a function is called a ____.",
-        choices: ["method", "string", "stylesheet", "boolean"],
-        answer: "method"
-    },
-    {
-        title: "Question 5: The logical operator that represents 'or' is ____.",
-        choices: ["||", "OR", "&&", "==="],
-        answer: "||"
     }
 ];
 // };
